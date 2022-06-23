5e90ed65199070c1a18e481b2d5d581c1ee989e4
Rene0115 <enere0115@gmail.com>
2022-06-23 11:40:09 +0100

    changes

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
 //jshint esversion:6
 const express = require("express");
-const bodyParser = requiure("body-parser"); 
+const bodyParser = require("body-parser"); 
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 
 const app = express();
 
 app.use(express.static("public"));
 app.set ("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
+
+app.get("/", function (req, res) {
+    res.render("home");
+});
+
+app.get("/login", function (req, res) {
+    res.render("login");
+});
+
+app.get("/register", function (req, res) {
+    res.render("register");
+});
 
 
 
